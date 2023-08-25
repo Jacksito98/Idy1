de24cd5d7421c1e5d60e715d34cf5e0bbec365b5
Jack <80754943+Jacksito98@users.noreply.github.com>
2023-08-24 22:26:52 -0400

    Update config.js

--- config.js
+++ config.js
@@ -34,6 +34,10 @@ global.fgyt = 'https://youtube.com'
 global.fgpyp = 'https://paypal.me/Jacksito23'
 global.fglog = 'https://i.imgur.com/Owmb93c.png' 
 
+//globals
+
+global.nyanregis = fs.readFileSync('./src/nyanregis.jpg')
+
 global.wait = '*⌛ _Cargando..._*\n*▰▰▰▱▱▱▱▱*'
 global.rwait = '⌛'
 global.dmoji = '🤭'
