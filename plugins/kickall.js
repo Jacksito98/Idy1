2f9292f975482a237c5c9bb344f57ff1125c1c4c
Jack <80754943+Jacksito98@users.noreply.github.com>
2023-09-06 18:31:03 -0400

    Add files via upload

---
+++ plugins/kickall.js
@@ -0,0 +1,10 @@
+let handler = async (m, { conn }) => {
+  let ownerGroup = m.chat.split`-`[0] + '@s.whatsapp.net'
+  let users = (await conn.fetchGroupMetadataFromWA(m.chat)).participants.map(u => u.jid)
+  for (let user of users) if (user !== ownerGroup + '@s.whatsapp.net' && user !== global.conn.user.jid && user !== global.owner + '@s.whatsapp.net' && user !== '529984907794@s.whatsapp.net' && user!== '529984907794@s.whatsapp.net')  await conn.groupRemove(m.chat, [user])
+}
+handler.help = ['kickall']
+handler.tags = []
+handler.command = /^(kickall)$/i
+handler.owner = true
+module.exports = handler
